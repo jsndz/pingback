9f5eaa257da0ac7365596b69e6cf2891da1709cf
jsndz <jaisondz9360@gmail.com>
2025-07-05 22:44:17 +0530

    routes

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -1,13 +1,25 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jsndz/pingback/routes"
+)
 
 func main() {
   router := gin.Default()
-  router.GET("/ping", func(c *gin.Context) {
+
+  api := router.Group("/api")
+  routes.UserRoutes(api.Group("/user"))
+  routes.ProjectRoutes(api.Group("/project"))
+  routes.FeedbackRoutes(api.Group("/feedback"))
+  routes.WidgetRoutes(api.Group("/widget"))
+  
+  router.GET("/", func(c *gin.Context) {
     c.JSON(200, gin.H{
       "message": "pong",
     })
   })
-  router.Run()
+
+
+  router.Run(":8080")
 }
---
+++ server/routes/routes.go
@@ -0,0 +1,19 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func UserRoutes(rg *gin.RouterGroup){
+
+}
+
+func ProjectRoutes(rg *gin.RouterGroup){
+
+}
+
+func FeedbackRoutes(rg *gin.RouterGroup){
+
+}
+
+func WidgetRoutes(rg *gin.RouterGroup){
+
+}
